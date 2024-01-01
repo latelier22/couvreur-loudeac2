e6c62142702205974b9e4fdb7aef62afcb63fe86
Cyrille LE CORRE <latelier.cyrille@gmail.com>
2024-01-01 09:51:16 +0100

    sitemap.js et logo caché en mobile

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import Footer from './Footer';
 const Home = () => {
   // Dynamic metadata for the home page
   const pageTitle = 'Accueil';
-  const pageDescription = 'Description de la page d\'accueil';
+  const pageDescription = 'Bienvenue sue le site de PRO COUVERTURE LOUDEAC';
 
   return (
     <RootLayout pageTitle={pageTitle} pageDescription={pageDescription}>
